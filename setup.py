8081aa1619f3322f6e5dea70b092e0d4fb746b4c
Andy Lu <andy@stitchdata.com>
2019-05-09 13:59:08 +0000

    Fix error in install-requires, add extras, update author and url

--- setup.py
+++ setup.py
@@ -5,15 +5,22 @@ setup(
     name="tap-gemini",
     version="0.1.0",
     description="Singer.io tap for extracting data from Yahoo Gemini",
-    author="Joe Heffer",
-    url="https://bitbucket.org/Joe_Heffer/tap-gemini",
+    author="Stitch",
+    url="http://singer.io",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_gemini"],
     install_requires=[
-        "singer-python=5.4.1",
-        "requests=2.21.0",
-        "pytz=2018.4",
+        "singer-python==5.4.1",
+        "requests==2.21.0",
+        "pytz==2018.4",
     ],
+    extras_require={
+        'dev': [
+            'ipdb==0.11',
+            'pylint==2.1.1',
+            'astroid==2.1.0',
+        ]
+    },
     entry_points="""
     [console_scripts]
     tap-gemini=tap_gemini:main
