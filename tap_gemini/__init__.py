aa76b12643bf584bc872dcd3e978b72259a098fd
Joe Heffer <joe.heffer@mypensionexpert.com>
2019-11-04 09:27:02 +0000

    Disable load_key_properties

--- tap_gemini/__init__.py
+++ tap_gemini/__init__.py
@@ -164,7 +164,9 @@ def discover() -> singer.Catalog:
 
     raw_schemas = load_schemas()
     metadata = load_metadata()
-    key_properties = load_key_properties()
+    # Disable key properties, which aren't currently used
+    # key_properties = load_key_properties()
+    key_properties = dict()
 
     streams = list()
 
