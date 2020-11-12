53235d4e96ed5f0a90fba47fc910506e99a045bb
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2020-11-12 13:13:26 +0100

    fixing gemini window issue

--- tap_gemini/settings.py
+++ tap_gemini/settings.py
@@ -19,16 +19,31 @@ MAX_WINDOW_DAYS = dict(
     keyword_stats=400,
     product_ads=400,
     site_performance_stats=400,
+    conversion_rules_stats=400,
+    domain_performance_stats=400,
+    call_extension_stats=400,
+    ad_extension_details=400,
+    adjustment_stats=400,
+    product_ad_performance_stats=400,
+    user_stats=400
 )
 
 # Maximum number of days to go back in time
 # This prevents ERROR_CODE:10002 Max look back window exceeded expected
 MAX_LOOK_BACK_DAYS = dict(
+    search_stats=15,
     performance_stats=15,
     slot_performance_stats=15,
     product_ads=400,
     site_performance_stats=400,
     keyword_stats=750,
+    conversion_rules_stats=400,
+    domain_performance_stats=400,
+    call_extension_stats=400,
+    ad_extension_details=400,
+    adjustment_stats=400,
+    product_ad_performance_stats=400,
+    user_stats=400
 )
 
 BOOKMARK_KEY = 'start_date'
